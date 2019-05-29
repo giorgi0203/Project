d87cd30be14be1902714726efd3d43f6d7a5b56e
GiorgiArabuli <giorgi1997arabuli@gmail.com>
2019-05-29 16:03:24 +0400

    feat(Auth): Finish login designe

--- app/src/main/java/com/example/project/SplashScreen.kt
+++ app/src/main/java/com/example/project/SplashScreen.kt
@@ -1,7 +1,12 @@
 package com.example.project
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.project.module.auth.LoginActivity
+import java.util.*
+import kotlin.concurrent.schedule
 
 class SplashScreen : AppCompatActivity() {
 
@@ -17,5 +22,10 @@ class SplashScreen : AppCompatActivity() {
         //check for remeber tocken
         //open login activity or dashboard
 
+        Handler().postDelayed({
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }, 2000)
     }
 }
